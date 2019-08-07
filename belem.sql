7fb2299d122608d743218123d800fc4b23582971
Samuel <30638348+samuel0072@users.noreply.github.com>
2019-08-07 15:09:11 -0300

    Add files via upload

---
+++ belem.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 07, 2019 at 08:09 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.1.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `belem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classmember`
+--
+
+CREATE TABLE `classmember` (
+  `id` int(11) NOT NULL,
+  `schoolmember` int(11) NOT NULL,
+  `classid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grade`
+--
+
+CREATE TABLE `grade` (
+  `id` int(11) NOT NULL,
+  `gradeNumber` int(11) NOT NULL,
+  `schoolId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `GradeClass`
+--
+
+CREATE TABLE `GradeClass` (
+  `id` int(11) NOT NULL,
+  `gradeId` int(11) NOT NULL,
+  `teacherEnroll` int(11) NOT NULL,
+  `classLetter` varchar(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `school`
+--
+
+CREATE TABLE `school` (
+  `id` int(11) NOT NULL,
+  `name` varchar(300) DEFAULT NULL,
+  `principal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schoolmember`
+--
+
+CREATE TABLE `schoolmember` (
+  `enrollnumber` int(11) NOT NULL,
+  `name` varchar(300) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `gender` enum('masculino','feminio') DEFAULT NULL,
+  `schoolid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `classmember`
+--
+ALTER TABLE `classmember`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `schoolmember` (`schoolmember`),
+  ADD KEY `classid` (`classid`);
+
+--
+-- Indexes for table `grade`
+--
+ALTER TABLE `grade`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `schoolId` (`schoolId`);
+
+--
+-- Indexes for table `GradeClass`
+--
+ALTER TABLE `GradeClass`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `gradeId` (`gradeId`),
+  ADD KEY `teacherEnroll` (`teacherEnroll`);
+
+--
+-- Indexes for table `school`
+--
+ALTER TABLE `school`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `principal` (`principal`);
+
+--
+-- Indexes for table `schoolmember`
+--
+ALTER TABLE `schoolmember`
+  ADD PRIMARY KEY (`enrollnumber`),
+  ADD KEY `schoolid` (`schoolid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `classmember`
+--
+ALTER TABLE `classmember`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `grade`
+--
+ALTER TABLE `grade`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `GradeClass`
+--
+ALTER TABLE `GradeClass`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `school`
+--
+ALTER TABLE `school`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `classmember`
+--
+ALTER TABLE `classmember`
+  ADD CONSTRAINT `classmember_ibfk_1` FOREIGN KEY (`schoolmember`) REFERENCES `schoolmember` (`enrollnumber`),
+  ADD CONSTRAINT `classmember_ibfk_2` FOREIGN KEY (`classid`) REFERENCES `GradeClass` (`id`);
+
+--
+-- Constraints for table `grade`
+--
+ALTER TABLE `grade`
+  ADD CONSTRAINT `grade_ibfk_1` FOREIGN KEY (`schoolId`) REFERENCES `school` (`id`);
+
+--
+-- Constraints for table `GradeClass`
+--
+ALTER TABLE `GradeClass`
+  ADD CONSTRAINT `GradeClass_ibfk_1` FOREIGN KEY (`gradeId`) REFERENCES `grade` (`id`),
+  ADD CONSTRAINT `GradeClass_ibfk_2` FOREIGN KEY (`teacherEnroll`) REFERENCES `schoolmember` (`enrollnumber`);
+
+--
+-- Constraints for table `school`
+--
+ALTER TABLE `school`
+  ADD CONSTRAINT `principal` FOREIGN KEY (`principal`) REFERENCES `schoolmember` (`enrollnumber`);
+
+--
+-- Constraints for table `schoolmember`
+--
+ALTER TABLE `schoolmember`
+  ADD CONSTRAINT `schoolmember_ibfk_1` FOREIGN KEY (`schoolid`) REFERENCES `school` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
