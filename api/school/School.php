398bc3fd60b7e4e719675ee9745a519757c3e2e9
samuel0072 <sls@ic.ufal.br>
2019-08-12 16:30:21 -0300

    removido principal da classe school

--- api/school/School.php
+++ api/school/School.php
@@ -6,23 +6,15 @@ namespace api\School;
 
 class School
 {
-    private $id;
     private $name;
-    private $principal;
 
-    function __construct($name, $id = 0, $principal = 0)
+    function __construct($name, $id = 0)
     {
         $this->name = $name;
         $this->id = $id;
-        $this->principal = $principal;
     }
-    public function getId() {
-        return $this->id;
-    }
+
     public function getName() {
         return $this->name;
-    }
-    public function getPrincipal() {
-        return $this->principal;
     }
 }
