d666a5fd411e14769b99d29fe306172aa7473181
samuel <sls@ic.ufal.br>
2019-08-19 15:05:13 -0300

    modificações nos resources

--- index.php
+++ index.php
@@ -7,17 +7,34 @@ include_once __DIR__ . '/api/routes/Request.php';
 
 include_once __DIR__ . '/api/resources/schoolmember.php';
 include_once __DIR__ . '/api/resources/school.php';
+include_once __DIR__.'/api/resources/grade.php';
 
 use \routes\Router as Router;
 use \routes\Request as Request;
 
 $router = new Router(new Request);
 
-$router->post('/belem/', function($request) {
-    echo insert_School($request->getBody());
-});
+$router->post('/belem/school/',
+    function($request) {
+        echo insert_School($request->getBody());
+    }
+);
 
-$router->get('/belem/get/:id', function($request) {
-    echo "funcionando";
-  // echo $request["params"]["id"];
-})->getURLParams('/belem/get/:id');
+$router->post('/belem/class/',
+    function ($request) {
+        $var = $request->getBody();
+        echo insert_class(...$var);
+    }
+);
+
+$router->post('/belem/grade/',
+    function($request) {
+        $params = $request->getBody();
+        echo insert_grade(...$params);
+    }
+);
+$router->get('/belem/get/',
+    function($request) {
+        echo "funcionando";
+    }
+);
