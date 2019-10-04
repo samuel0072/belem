2760f6ccfaa4f9d24722714949efd6e62aaa39ef
samuel <sls@ic.ufal.br>
2019-10-03 21:24:27 -0300

    pequenas mudanças no bd :'(

---
+++ app/Http/Controllers/UserGradeClassController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserGradeClass;
+use Illuminate\Http\Request;
+
+class UserGradeClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UserGradeClass  $userGradeClass
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserGradeClass $userGradeClass)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UserGradeClass  $userGradeClass
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserGradeClass $userGradeClass)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UserGradeClass  $userGradeClass
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserGradeClass $userGradeClass)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UserGradeClass  $userGradeClass
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserGradeClass $userGradeClass)
+    {
+        //
+    }
+}
