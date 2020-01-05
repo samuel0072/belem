87d00922b1b135c819818017ce1e2aa8f84d422f
Samuel <sls@ic.ufal.br>
2020-01-05 10:11:03 -0300

    melhorados comentarios

--- app/Http/Controllers/GradeClassController.php
+++ app/Http/Controllers/GradeClassController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use App\GradeClass;
 use App\Http\Requests\GradeClassRequest;
-
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
+use Illuminate\View\View;
 
 
 class GradeClassController extends Controller
 {
 
-    /*
+    /**
      * Mostra as classes do usuário. Se for admin mostra todas as classes
      *
-     *  @return view
+     * @return Factory|View
      * */
     public function index()
     {
@@ -29,22 +33,26 @@ class GradeClassController extends Controller
         return view('grade_class.show', compact('gradeClasses'));
     }
 
-    /*
+    /**
      * Retorna a classe
-     * @param $gradeClassId inteiro, id da classe
+     * @param int $gradeClassId , id da classe
      * @return App\GradeClass
      *
-    */
+     * @throws AuthorizationException
+     */
     public function show($gradeClassId){
         $gradeClass = GradeClass::findOrFail($gradeClassId);
         $this->authorize('view', $gradeClass);
         return $gradeClass;
     }
-    /*
-     *  cria uma classe no banco
-     *  @param App\Http\Requests\GradeClassrequest $request
-     *  @return redireciona
-     * */
+
+    /**
+     *  Cria uma classe no banco
+     * @param App\Http\Requests\GradeClassRequest $request
+     * @return RedirectResponse|Redirector
+     *
+     * @throws AuthorizationException
+     */
     public function store(GradeClassRequest $request)
     {
         $this->authorize('create', GradeClass::class);
@@ -55,12 +63,14 @@ class GradeClassController extends Controller
     }
 
 
-    /*
+    /**
      *  Atualiza as informações de uma classe
-     *  @param App\Http\Requests\GradeClassrequest $request
-     *  @param App\GradeClass $gradeClass
-     *  @return redireciona
-     * */
+     * @param GradeClassRequest $request
+     * @param GradeClass $gradeClass
+     * @return RedirectResponse|Redirector
+     *
+     * @throws AuthorizationException
+     */
     public function update(GradeClassRequest $request, GradeClass $gradeClass)
     {
         $this->authorize('update', $gradeClass);
@@ -70,20 +80,23 @@ class GradeClassController extends Controller
         return redirect("/school/$id/classes");//Mostra as classes da escola
     }
 
-    /*
+    /**
      *  Renderiza uma view de criação de classes
-     *  @return view
-     * */
+     * @return Factory|View
+     *
+     * @throws AuthorizationException
+     */
     public function create(){
         $this->authorize('create', GradeClass::class);
         return view('grade_class.create');
     }
 
-    /*
+    /**
      *  Deleta a classe
-     *  @param App\GradeClass $gradeClass
-     *  @return redireciona
-     * */
+     * @param GradeClass $gradeClass
+     * @return RedirectResponse|Redirector
+     * @throws AuthorizationException
+     */
     public function destroy(GradeClass $gradeClass)
     {
         $this->authorize('delete', $gradeClass);
@@ -92,11 +105,13 @@ class GradeClassController extends Controller
         return redirect("/school/$id/classes");
     }
 
-    /*
+    /**
      *  Retorna os estudantes da classe(school_members)
-     *  @param int $id, id da classe
-     *  @return array App\SchoolMember
-     * */
+     * @param int $id , id da classe
+     * @return array App\SchoolMember
+     *
+     * @throws AuthorizationException
+     */
     public function classMembers($id) {
         $class = GradeClass::findOrFail($id);
         $this->authorize('view', $class);//mesma logica de ver os dados de uma classe
@@ -104,11 +119,13 @@ class GradeClassController extends Controller
         return $students;
     }
 
-    /*
+    /**
      *  Retorna os testes associados a uma classe
-     *  @param int $id, id da classe
-     *  @return array App\Test
-     * */
+     * @param int $id , id da classe
+     * @return array App\Test
+     *
+     * @throws AuthorizationException
+     */
 
     public function tests($id) {
         $class = GradeClass::findOrFail($id);
@@ -116,21 +133,25 @@ class GradeClassController extends Controller
         return $class->tests;
     }
 
-    /*
+    /**
      *  Renderiza uma view com os estudantes da clsse
-     *  @param int $id, id da classe
-     *  @return view
-     * */
+     * @param int $id , id da classe
+     * @return Factory|View
+     *
+     * @throws AuthorizationException
+     */
     public function showClassMembers($id) {
         $students = $this->classMembers($id);
         return view("school_member.showAll", compact(['students', 'id']));
     }
 
-    /*
+    /**
      *  Renderiza uma view com os testes da clsse
-     *  @param int $id, id da classe
-     *  @return view
-     * */
+     * @param int $id , id da classe
+     * @return Factory|View
+     *
+     * @throws AuthorizationException
+     */
     public function showTests($id) {
         $tests = $this->tests($id);
         return view("test.showAll", compact(['tests', 'id']));
